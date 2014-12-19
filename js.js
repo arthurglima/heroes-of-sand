227e85d8eb235cd112ddcce6e4e36b271535d987
Vitor Lima <vgdlima@gmail.com>
2014-12-19 12:03:11 -0300

    Retornando null

--- js.js
+++ js.js
@@ -2,6 +2,7 @@
 var battle = function (forca, armadura){
 	if (typeof(forca)!="number" || typeof(armadura)!="number"){
 		alert("Valores devem ser inteiros");
+		return null;
 	}
 	
 	if(forca > armadura){
